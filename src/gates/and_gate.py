bf537514aa57dc983cded3501cc3a2e0c9e73563
Taeho Kim <tk3718@ic.ac.uk>
2022-10-22 01:20:30 +0100

    Implementaion of AND gate with perceptron

---
+++ src/gates/and_gate.py
@@ -0,0 +1,13 @@
+import imp
+from perceptron.perceptron import Perceptron
+
+neuron = Perceptron(2)
+
+"""Implementation of AND gate."""
+neuron.set_weights([10, 10, -15])
+
+print("Gate:")
+print ("0 0 = {0:.10f}".format(neuron.run([0,0])))
+print ("0 1 = {0:.10f}".format(neuron.run([0,1])))
+print ("1 0 = {0:.10f}".format(neuron.run([1,0])))
+print ("1 1 = {0:.10f}".format(neuron.run([1,1])))
